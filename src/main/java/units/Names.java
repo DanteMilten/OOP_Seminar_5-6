a447385fe8a39f9a1b672ad3cc7c9f1255462122
DanteMilten <milten47@yahoo.com>
2023-07-23 15:32:44 +0300

    Initial commit

---
+++ src/main/java/units/Names.java
@@ -0,0 +1,14 @@
+package hero;
+
+public class Names {package hero;
+
+    // 50 Имен
+    public enum Names {
+        Liam, Emma, Noah, Olivia, William, Ava, James, Isabella, Oliver, Kirill,
+        Benjamin, Mia, Elijah, Charlotte, Lucas, Solovei, Mason, Harper, Logan,
+        Evelyn, Alexander, Abigail, Ermak, Emily, Jacob, Zaitsev, Michael, Sofia,
+        Daniel, Madison, Henry, Avery, Jackson, Ella, Harry, Scarlett, Aiden,
+        Chloe, Matthew, Victoria, Samuel, Grace, David, Zoey, Aleksandr, Penelope,
+        Carter, Lila, Owen, Ronan
+    }
+}
